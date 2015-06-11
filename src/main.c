00948251138bf86daf43e8e391c04ce87465c798
Ben James <benjamin-james@users.noreply.github.com>
2015-06-10 19:00:52 -0500

    Created some necessary files

---
+++ src/main.c
@@ -0,0 +1,16 @@
+#include "SDL2/SDL.h"
+
+/*
+ * Let's please use documentation
+ * above the function rather than
+ * inside it
+ */
+int main(int argc, char **argv)
+{
+	SDL_Init(SDL_INIT_VIDEO);
+	SDL_Window *w = SDL_CreateWindow(*argv, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, 0);
+	SDL_Delay(5000);
+	SDL_DestroyWindow(w);
+	SDL_Quit();
+	return 0;
+}
