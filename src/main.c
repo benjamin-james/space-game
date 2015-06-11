801396da85e6385be09e19d0056a37a30a1a8818
Ben James <benjamin-james@users.noreply.github.com>
2015-06-10 19:18:09 -0500

    Following autotools conventions

--- src/main.c
+++ src/main.c
@@ -7,8 +7,9 @@
  */
 int main(int argc, char **argv)
 {
+	SDL_Window *w;
 	SDL_Init(SDL_INIT_VIDEO);
-	SDL_Window *w = SDL_CreateWindow(*argv, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, 0);
+	w = SDL_CreateWindow(*argv, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, 0);
 	SDL_Delay(5000);
 	SDL_DestroyWindow(w);
 	SDL_Quit();
