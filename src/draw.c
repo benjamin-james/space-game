f1114cf7f37e683001cb6cb89e61ca2dc2724daa
Benjamin James <benjamin-james@users.noreply.github.com>
2015-06-12 11:37:07 -0500

    cleanup potential bugs

--- src/draw.c
+++ src/draw.c
@@ -6,20 +6,23 @@
 #include "draw.h"
 
 /*
- * Draws a rectangle in my
- * preferred style, with an
- * outline.
+ * Draws a rectangle
+ * with color
  */
 void draw_rect(SDL_Renderer *r, Uint32 color, int x, int y, int w, int h)
 {
 	SDL_Rect current = {x,y,w,h};
 	SDL_SetRenderDrawColor(r, (color >> 24) & 0xFF, (color >> 16) & 0xFF, (color >> 8) & 0xFF, 0xFF);
         SDL_RenderFillRect(r, &current);
-	SDL_SetRenderDrawColor(r, 0, 0, 0, 0xFF);
-	SDL_RenderDrawRect(r, &current);
+/*	SDL_SetRenderDrawColor(r, 0, 0, 0, 0xFF);
+	SDL_RenderDrawRect(r, &current);*/
 }
 
 /*
+ * Draws the grid at zoom g->zoom and
+ * centered at (g->cx, g->cy) on the grid
+ * coordinate system.
+ *
  * This function is in need of cleanup
  * especially for errors.
  * The 'for' loops seem duplicated,
@@ -31,20 +34,20 @@ void render(struct game *g)
 {
 	double tile_size_x = g->width * g->zoom / (GRID_NORMAL_SIZE * 100.0);
 	double tile_size_y = g->height * g->zoom / (GRID_NORMAL_SIZE * 100.0);
-	double num_tiles_w = g->width / tile_size_x;
-	double num_tiles_h = g->height / tile_size_y;
+	double num_tiles_w = GRID_NORMAL_SIZE * 100.0 / g->zoom;
+	double num_tiles_h = GRID_NORMAL_SIZE * 100.0 / g->zoom;
 
 	int i,j, sx, sy;
 	for (i = 0, sx = (g->width - tile_size_x) / 2; i < (num_tiles_w + 1)/ 2; i++, sx += tile_size_x) {
 		for (j = 0, sy = (g->height - tile_size_y) / 2; j < (num_tiles_h + 1) / 2; j++, sy += tile_size_y)
-			draw_rect(g->renderer, g->grid[g->cx + i][g->cy + j], sx-1, sy-1, round(tile_size_x)+1, round(tile_size_y)+1);
+			draw_rect(g->renderer, g->grid[g->cx + i][g->cy + j], sx-1, sy-1, round(tile_size_x)+2, round(tile_size_y)+2);
 		for (j = 0, sy = (g->height - tile_size_y) / 2; j < (num_tiles_h + 1) / 2; j++, sy -= tile_size_y)
-			draw_rect(g->renderer, g->grid[g->cx + i][g->cy - j], sx-1, sy-1, round(tile_size_x)+1, round(tile_size_y)+1);
+			draw_rect(g->renderer, g->grid[g->cx + i][g->cy - j], sx-1, sy-1, round(tile_size_x)+2, round(tile_size_y)+2);
 	}
 	for (i = 0, sx = (g->width - tile_size_x) / 2; i < (num_tiles_w + 1)/ 2; i++, sx -= tile_size_x) {
 		for (j = 0, sy = (g->height - tile_size_y) / 2; j < (num_tiles_h + 1) / 2; j++, sy += tile_size_y)
-			draw_rect(g->renderer, g->grid[g->cx - i][g->cy + j], sx-1, sy-1, round(tile_size_x)+1, round(tile_size_y)+1);
+			draw_rect(g->renderer, g->grid[g->cx - i][g->cy + j], sx-1, sy-1, round(tile_size_x)+2, round(tile_size_y)+2);
 		for (j = 0, sy = (g->height - tile_size_y) / 2; j < (num_tiles_h + 1) / 2; j++, sy -= tile_size_y)
-			draw_rect(g->renderer, g->grid[g->cx - i][g->cy - j], sx-1, sy-1, round(tile_size_x)+1, round(tile_size_y)+1);
+			draw_rect(g->renderer, g->grid[g->cx - i][g->cy - j], sx-1, sy-1, round(tile_size_x)+2, round(tile_size_y)+2);
 	}
 }
